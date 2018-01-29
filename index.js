231fb767dcbe3773394a6c321551d8bdf6041c45
Marciel Torres <marcielribeirotorres@gmail.com>
2018-01-29 15:01:28 -0200

    improvement on express server port for heroku

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 
 const app = express();
 
+// set the port of application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
 app.get('/', function (req, res) {
     res.send('Hello World!');
   });
   
 
-app.listen(8080, function () {
+app.listen(port, function () {
     console.log('Server on')
 });
