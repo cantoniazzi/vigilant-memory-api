0ee56f3bd2f684481e8fc7096bd9696b969ced48
Marciel Torres <marcielribeirotorres@gmail.com>
2018-01-29 15:38:18 -0200

    Test connection with pg on heroku

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Client } = require('pg');
 
 const app = express();
 
@@ -8,8 +9,22 @@ var port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
+});
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
   });
   
+  client.connect();
+  
+  client.query('SELECT * FROM test;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+  });  
 
 app.listen(port, function () {
     console.log('Server on')
